d0f91eb92aa3f5e70848dd2e740f246b1bc6f506
juliodacunha <c.juliodacunha@gmail.com>
2019-06-28 18:12:54 -0300

    Cadastro de usuário funcional com multiple tables

--- ci-pfc/application/controllers/Cadastro_usuario.php
+++ ci-pfc/application/controllers/Cadastro_usuario.php
@@ -2,7 +2,8 @@
 
 class Cadastro_usuario extends CI_Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         parent:: __construct();
         $this->load->helper('url');
     }
@@ -14,18 +15,65 @@ class Cadastro_usuario extends CI_Controller
         $this->load->view('footer');
     }
 
-    public function cadastrar(){
-        $data['tipuser_tip_user '] = $this->input->post('tipuser_tip_user ');
-        $data['cpf'] = $this->input->post('cpf');
-        $data['rg'] = $this->input->post('rg');
-        $data['email'] = $this->input->post('email');
-        $data['sexo'] = $this->input->post('sexo');
-        $data['nome'] = $this->input->post('nome');
-        $data['sobrenome'] = $this->input->post('sobrenome');
-        $data['senha'] = md5($this->input->post('senha'));
+//    public function cadastrar(){
+//        $data['tipuser_tip_user '] = $this->input->post('tipuser_tip_user ');
+//        $data['cpf'] = $this->input->post('cpf');
+//        $data['rg'] = $this->input->post('rg');
+//        $data['email'] = $this->input->post('email');
+//        $data['sexo'] = $this->input->post('sexo');
+//        $data['nome'] = $this->input->post('nome');
+//        $data['sobrenome'] = $this->input->post('sobrenome');
+//        $data['senha'] = md5($this->input->post('senha'));
+//
+//        if($this->db->insert('usuarios',$data)){
+//            redirect('Cadastro_usuario');
+//        }
+//    }
 
-        if($this->db->insert('usuarios',$data)){
-            redirect('Cadastro_usuario');
+
+
+    public function cadastrarUsuario(){
+        $this->load->model('Cadastro_model', 'model_cadastro', TRUE);
+        $tipo_usuario = array();
+        $usuario_input = array();
+        $endereco_input = array();
+        $passageiro_input = array();
+
+        $usuario_input['tipuser_tip_user'] = $this->input->post('tipuser_tip_user');
+        $usuario_input['nome'] = $this->input->post('nome');
+        $usuario_input['sobrenome'] = $this->input->post('sobrenome');
+        $usuario_input['senha'] = md5($this->input->post('senha'));
+        $usuario_input['rg'] = $this->input->post('rg');
+        $usuario_input['cpf'] = $this->input->post('cpf');
+        $usuario_input['email'] = $this->input->post('email');
+        $usuario_input['telefone'] = $this->input->post('telefone');
+        $usuario_input['sexo'] = $this->input->post('sexo');
+
+        $passageiro_input['emp_cod_empresa'] = $this->input->post('emp_cod_empresa');
+        $passageiro_input['turma'] = $this->input->post('turma');
+        $passageiro_input['curso'] = $this->input->post('curso');
+        $passageiro_input['matricula'] = $this->input->post('matricula');
+
+        $endereco_input['cep'] = $this->input->post('cep');
+        $endereco_input['rua'] = $this->input->post('rua');
+        $endereco_input['numero'] = $this->input->post('numero');
+        $endereco_input['complemento'] = $this->input->post('complemento');
+        $endereco_input['bairro'] = $this->input->post('bairro');
+        $endereco_input['cidade'] = $this->input->post('cidade');
+        $endereco_input['estado'] = $this->input->post('estado');
+
+
+
+
+
+
+        $checking_insert = $this -> model_cadastro -> cadastro_usuario_form($usuario_input, $passageiro_input, $endereco_input);
+        if($checking_insert){
+            redirect(base_url('Login.php'));
+        }else{
+            redirect(base_url('Login.php'));
         }
+
     }
 }
+
--- ci-pfc/application/views/cadastro_usuario.php
+++ ci-pfc/application/views/cadastro_usuario.php
@@ -1,13 +1,188 @@
-
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Gerenciamento de vans</title>
+    <title>Cadastro</title>
 </head>
-
 <!-- PARTE DO CADASTRO -->
+
+<div class="col-md-12 register-right">
+    <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Aluno</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Motorista</a>
+        </li>
+    </ul>
+    <!-- REGISTRO DE ALUNO -->
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <form action="<?= base_url() ?>Cadastro_usuario/cadastrarUsuario" method="post">
+            <h3 class="register-heading">Cadastre-se como um aluno</h3>
+                        <div class="row register-form">
+                        <input type="hidden" id="tipuser_tip_user" name="tipuser_tip_user" value="1">
+                        <input type="hidden" id="emp_cod_empresa" name="emp_cod_empresa" value="1">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Primeiro nome *" id="nome" name="nome" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Ultimo nome *" id="sobrenome" name="sobrenome" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Senha *" id="senha" name="senha"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="RG *" id="rg" name="rg" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="CPF *" id="cpf" name="cpf" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email *" id="email" name="email"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="CEP *" id="cep" name="cep"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Rua *" id="rua" name="rua" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Número da residência *" id="numero" name="numero" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Complemento *" id="complemento" name="complemento" />
+                    </div>
+
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Bairro *" id="bairro" name="bairro" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Cidade *" id="cidade" name="cidade" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Estado *" id="estado" name="estado" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" minlength="10" maxlength="11" name="telefone" class="form-control" placeholder="Telefone *" id="telefone" name="telefone" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" minlength="3" maxlength="10" name="turma" class="form-control" placeholder="Turma *" id="turma" name="turma" />
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input type="text" minlength="4" maxlength="15" name="curso" class="form-control" placeholder="Curso *" id="curso" name="curso" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" minlength="10" maxlength="10" name="matricula" class="form-control" placeholder="Matricula *" id="matricula" name="matricula" />
+                    </div>
+                    <div class="form-group">
+                        <div class="maxl">
+                            <label class="radio inline">
+                                <input type="radio" name="sexo" id="sexo" value="Masculino" checked>
+                                <span> Masculino </span>
+                            </label>
+                            <label class="radio inline">
+                                <input type="radio" name="sexo" id="sexo" value="Feminino">
+                                <span>Feminino </span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <input type="submit" class="btnRegister" style="float: right"  value="Registrar"/>
+
+        </form>
+            <!-- FIM DO REGISTRO DE USUÁRIO -->
+            <!-- REGISTRO DE MOTORISTA -->
+        </div>
+        <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <h3 class="register-heading">Cadastre-se como um motorista</h3>
+            <div class="row register-form">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Primeiro nome *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Ultimo nome *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Senha *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="RG *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="CPF *" value="" />
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Rua *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Número da residência *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Complemento *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Bairro *" value="" />
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Cidade *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Estado *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Telefone *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" minlength="10" maxlength="10" name="cnh" class="form-control" placeholder="CNH *" value="" />
+                    </div>
+
+                    <div class="form-group">
+                        <div class="maxl">
+                            <label class="radio inline">
+                                <input type="radio" name="gender" value="Masculino" checked>
+                                <span> Masculino </span>
+                            </label>
+                            <label class="radio inline">
+                                <input type="radio" name="gender" value="Feminino">
+                                <span>Feminino </span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <input type="submit" class="btnRegister" style="float: right"  value="Registrar"/>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+    <!--
 <div class="container">
     <div class="card w-50 mx-auto my-5">
         <article class="card-body mx-auto" style="">
