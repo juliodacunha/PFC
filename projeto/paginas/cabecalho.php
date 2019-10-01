9514f41781057e8bed9208fb943433b2c5ddab18
Julio da Cunha <c.juliodacunha@gmail.com>
2019-10-01 19:48:18 -0300

    Dados do banco para o perfil.php

--- projeto/paginas/cabecalho.php
+++ projeto/paginas/cabecalho.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 include("../funcoes/Conexao.php");
 require("head.php");
 ?>
--- projeto/paginas/perfil.php
+++ projeto/paginas/perfil.php
@@ -2,7 +2,18 @@
 include('../funcoes/Verifica_login.php');
 require("head.php");
 require("cabecalho.php");
+
+//Abaixo o código de busca no banco de dados sobre o formulário de perfil do passageiro
+$email = $_SESSION['email'];
+$result = $conexao->query("SELECT nome, sobrenome, cpf, rg, telefone, email, senha, curso, turma, matricula, cep, rua, numero, bairro, cidade, estado, complemento
+from usuarios, passageiros, enderecos 
+where email = '$email' and id_usuario_id = id_usuario and id_passageiro = id_passageiro_id") or die($conexao->error);
+
+//Abaixo um teste: apenas executar quando estiver codando
+//print_r($resultPass);
+//print_r($result->fetch_assoc());
 ?>
+
 <html lang="en">
 <head>
     <title>Meu perfil</title>
@@ -11,14 +22,9 @@ require("cabecalho.php");
 <!-- PARTE DA PÁGINA -->
 <div class="container">
       <div class="row">
-
-           
        <br>
-
       </div>
         <div class="toppad" >
-   
-   
           <div class="panel panel-info">
             <div class="panel-heading">
               <h3 class="panel-title my-3 mb-5">NOME USUÁRIO</h3>
@@ -26,92 +32,81 @@ require("cabecalho.php");
             <div class="panel-body">
               <div class="row">
                 <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="#" class="img-circle img-responsive"> </div>
-                
-                <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
-                  <dl>
-                    <dt>DEPARTMENT:</dt>
-                    <dd>Administrator</dd>
-                    <dt>HIRE DATE</dt>
-                    <dd>11/12/2013</dd>
-                    <dt>DATE OF BIRTH</dt>
-                       <dd>11/12/2013</dd>
-                    <dt>GENDER</dt>
-                    <dd>Male</dd>
-                  </dl>
-                </div>-->
+                <?php $row = $result->fetch_assoc() ?>
                 <div class=" col-md-9 col-lg-9 "> 
                   <table class="table table-user-information">
                     <tbody>
                       <tr>
                         <td>Nome:</td>
-                        <td>Dados do banco</td>
+                        <td><?php echo $row['nome']; ?></td>
                       </tr>
                       <tr>
                         <td>Sobrenome:</td>
-                        <td>Dados do banco</td>
+                        <td><?php echo $row['sobrenome']; ?></td>
                       </tr>
                       <tr>
                         <td>CPF</td>
-                        <td>Dados do banco</td>
+                        <td><?php echo $row['cpf']; ?></td>
                       </tr>
                    
                          <tr>
                              <tr>
                         <td>RG</td>
-                        <td>Dados do banco</td>
+                        <td><?php echo $row['rg']; ?></td>
                       </tr>
                         <tr>
                         <td>Telefone</td>
-                        <td>Dados do banco</td>
+                        <td><?php echo $row['telefone']; ?></td>
                       </tr>
                       <tr>
                         <td>Email</td>
-                        <td>Dados do banco</a></td>
+                        <td><?php echo $row['email']; ?></a></td>
                       </tr>
                       <tr>
                         <td>Senha</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['senha']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Curso</td>
-                        <td>Dados do banco</a></td>
+                        <td><?php echo $row['curso']; ?></a></td>
                       </tr>
                       <tr>
                         <td>Turma</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['turma']; ?></td>                           
                       </tr>
                       </tr>
                         <td>Matrícula</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['matricula']; ?></td>                           
                       </tr>
                       <tr>
                         <td>CEP</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['cep']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Rua</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['rua']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Número</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['numero']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Bairro</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['bairro']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Cidade</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['cidade']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Estado</td>
-                        <td>Dados do banco</td>                           
+                        <td><?php echo $row['estado']; ?></td>                           
                       </tr>
                       <tr>
                         <td>Complemento</td>
-                        <td>Dados do banco</td>        
+                        <td><?php echo $row['complemento']; ?></td>        
                       </tr>
+                      <?php //endwhile; ?>
 
                     </tbody>
                   </table>
