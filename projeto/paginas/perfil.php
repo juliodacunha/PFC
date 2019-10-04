97b1b599bfee2c681a373faf5589588eb30c0fc5
Julio da Cunha <c.juliodacunha@gmail.com>
2019-10-04 13:08:40 -0300

    Revert "Update perfil.php"
    This reverts commit acbfc37f9e7469a6bc8271c6689121da4ca6a3a1.

--- projeto/paginas/perfil.php
+++ projeto/paginas/perfil.php
@@ -29,7 +29,6 @@ if (array_key_exists("cnh", $arrayMotorista)) {
   $validar = 0;
 }elseif(array_key_exists("matricula", $arrayPassageiro)){
   //echo "O usuário é um passageiro";
-  $row = $arrayPassageiro;
   $validar = 1;
 }
 
